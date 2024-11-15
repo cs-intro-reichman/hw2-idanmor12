b482a6adad600908f55ae8f2a5e9edd2c691fc3b
Idan Mor <idan.mor@post.runi.ac.il>
2024-11-15 09:43:36 +0200

    Finished Cheers

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,27 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String cheeringWord = args[0];
+                int repeatsNum = Integer.parseInt(args[1]);
+                String VowelSoundLetters = "AEFHILMNORSX";
+
+                for (int i = 0; i < cheeringWord.length(); i++) {
+                        char currLetter = cheeringWord.charAt(i);
+                        String indefiniteArticle= "a";
+                                for (int j = 0; j < VowelSoundLetters.length(); j++) {
+                                        if (VowelSoundLetters.indexOf(currLetter) != -1)
+                                        {
+                                                indefiniteArticle = "an";
+                                                break;
+                                        }
+                                }
+                        System.out.print("Give me " + indefiniteArticle + " " + currLetter + ": ");
+                        System.out.println(currLetter + "!");
+                }
+
+                System.out.println("What does that Spell?");
+                for (int i = 0; i < repeatsNum; i++) {
+                        System.out.println(cheeringWord + "!");
+                }
         }
 }
