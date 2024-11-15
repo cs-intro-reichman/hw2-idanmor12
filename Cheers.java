03bf0b1aa365e0fe3ee3ad88134b94fac7f07261
Idan Mor <idan.mor@post.runi.ac.il>
2024-11-15 11:17:28 +0200

    fixed according to autograder

--- Cheers.java
+++ Cheers.java
@@ -1,7 +1,7 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-                String cheeringWord = args[0];
+                String cheeringWord = args[0].toUpperCase();
                 int repeatsNum = Integer.parseInt(args[1]);
                 String VowelSoundLetters = "AEFHILMNORSX";
 
@@ -23,5 +23,6 @@ public class Cheers {
                 for (int i = 0; i < repeatsNum; i++) {
                         System.out.println(cheeringWord + "!!!");
                 }
+                System.out.println();
         }
 }
