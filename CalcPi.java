c02ba6988a7774c7870ec4d3231e1b9022b69f02
Idan Mor <idan.mor@post.runi.ac.il>
2024-11-15 10:19:32 +0200

    CalcPi finished

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,19 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+		double approximation = 1;
+		boolean isActionAddition = false;;
+		int currDenominator = 3;
+		for (int i = 0; i < Integer.parseInt(args[0])+1; i++, currDenominator+=2) {
+			if (isActionAddition){
+				approximation += (double)1/currDenominator;
+			} else {
+				approximation -= (double)1/currDenominator;
+			}
+			isActionAddition = !isActionAddition;
+		}
+		
+		System.out.println("pi accoriding to Java: " + Math.PI);
+		System.out.println("pi, approximated: " + (double)approximation * 4);
 	}
 }
