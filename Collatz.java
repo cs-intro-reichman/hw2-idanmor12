41bf2e35344bee7611da7a545483d2cb6f08550d
Idan Mor <idan.mor@post.runi.ac.il>
2024-11-15 10:55:54 +0200

    Collatz finished

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,31 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int numOfSequences = Integer.parseInt(args[0]);
+		boolean isVerbose= args[1].equals("v");
+		for (int i = 0; i < numOfSequences; i++) {
+			int startingNum = i + 1;
+			int counter = 1;
+			if (isVerbose) {
+				System.out.print(startingNum);
+			}
+			do {
+				if (startingNum % 2 == 0){
+					startingNum /= 2;
+				} else {
+					startingNum = startingNum * 3 + 1;
+				}
+				if (isVerbose){
+					System.out.print(", " + startingNum);
+				}
+				counter++;
+			} while (startingNum != 1);
+			if (isVerbose){
+				System.out.print(" (" + counter + ")");
+				System.out.println();
+			}
+		}
+		System.out.println("Every one of the first " +  numOfSequences + 
+					" hailstone sequences reached 1.");
 	}
 }
